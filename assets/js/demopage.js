642acdda1191241289d2a030e033f80211642e6e
pranaynaki <pranaynaki@gmail.com>
2017-09-11 14:40:28 -0500

    fixed demo

--- assets/js/demopage.js
+++ assets/js/demopage.js
@@ -10,7 +10,7 @@ $("#getreq").click(function(){
     geturl();
     if(url === "") url = "https://cdn.preterhuman.net/texts/literature/books_by_title/G%20-%20M/JK%20Rowling%20-%20(2)%20Harry%20Potter%20and%20the%20Chamber%20of%20Secrets%2087000%20words.txt";
     
-    $.get("http://54.172.144.234", {str: url}, function(data, status){
+    $.get("http://ec2-34-234-76-220.compute-1.amazonaws.com", {str: url}, function(data, status){
         $("#msg").html(data);
         $("#msg").show("slowly");
     });
