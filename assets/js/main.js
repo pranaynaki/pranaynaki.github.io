7ac19ad147eae393169ce3fcee686fd9d3907296
pranaynaki <pranaynaki@gmail.com>
2017-12-15 20:46:43 -0600

    google analytics idk y i forgot to add this a long time ago smh

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,15 @@
 	});
 
 })(jQuery);
+//stackoverflow
+var showText = function (target, message, index, interval) {   
+	if (index < message.length) {
+	  $(target).append(message[index++]);
+	  setTimeout(function () { showText(target, message, index, interval); }, interval);
+	}
+  }
+  $(function () {
+	
+	  showText("#msg", "Software Developer && Computer Science Student", 0, 75);   
+	
+	});
